912c3f09d08de8e72d73d9a5aaa8c31c67db9893
suhasumesh <suhasumesh2002@gmail.com>
2023-03-12 11:51:33 +0530

    2 Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res,next)=>{
+    res.status(200).send("Hello, testing Docker with node");
+});
+
+app.listen(8080,()=>{
+    console.log("Server Listening in port 8080");
+})

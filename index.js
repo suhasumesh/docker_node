240df3a5a02e2ee7bda028628115a9b61046a1bd
suhasumesh <suhasumesh2002@gmail.com>
2023-03-12 13:42:05 +0530

    Dockerized

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res,next)=>{
-    res.status(200).send("Hello, testing Docker with node");
+    res.status(200).send("Hello, testing Docker with node edited");
 });
 
 app.listen(8080,()=>{
